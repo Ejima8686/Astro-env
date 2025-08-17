a75ea6e3d9c5d24f5a264595d0b634f948165e55
Ejima8686 <ejima@EjimanoMacBook-Air.local>
2025-08-17 20:35:04 +0900

    feat:各種環境設定追加

---
+++ postcss.config.ts
@@ -0,0 +1,87 @@
+import type { Config } from 'postcss-load-config';
+
+/**
+ * レスポンシブデザインでビューポートサイズに応じてサイズが変化するカスタム関数
+ *
+ * @example
+ * --fluid(1rem, 2rem)
+ * // → clamp(1rem, 0.5rem + 1.5625svw, 2rem)
+ *
+ * @example
+ * --fluid(1rem, 2rem, 30rem, 120rem)
+ * // → clamp(1rem, 0.3333rem + 1.8519svw, 2rem)
+ *
+ * @example
+ * --fluid(16px, 32px)
+ * // → clamp(1rem, 0.5rem + 1.5625svw, 2rem)
+ */
+function fluid() {
+  const inputPattern = /^([+-]?[0-9]*\.?[0-9]+)(px|rem)$/;
+
+  function parseAsRem(input: string) {
+    const match = input.match(inputPattern);
+
+    if (!match) {
+      throw new Error(`${input} is an invalid input.`);
+    }
+
+    const [, value, unit] = match;
+    const divider = unit === 'px' ? 16 : 1;
+
+    return Number(value) / divider;
+  }
+
+  function round(n: number) {
+    return Math.round((n + Number.EPSILON) * 10000) / 10000;
+  }
+
+  return function fluidImpl(
+    minSize: string,
+    maxSize: string,
+    minBreakpoint = '640px',
+    maxBreakpoint = '1440px',
+    ...rest: unknown[]
+  ) {
+    if (!minSize || !maxSize) {
+      throw new Error(
+        'The --fluid(…) function requires at least 2 arguments, but received insufficient arguments.',
+      );
+    }
+
+    if (rest.length > 0) {
+      throw new Error(
+        `The --fluid(…) function only accepts 4 arguments, but received ${rest.length + 1}.`,
+      );
+    }
+
+    const minSizeRem = parseAsRem(minSize);
+    const maxSizeRem = parseAsRem(maxSize);
+    const minBreakpointRem = parseAsRem(minBreakpoint);
+    const maxBreakpointRem = parseAsRem(maxBreakpoint);
+
+    const slope = (maxSizeRem - minSizeRem) / (maxBreakpointRem - minBreakpointRem);
+    const intersection = -1 * minBreakpointRem * slope + minSizeRem;
+
+    return `clamp(${[
+      `${Math.min(minSizeRem, maxSizeRem)}rem`,
+      `${round(intersection)}rem + ${round(slope * 100)}svw`,
+      `${Math.max(minSizeRem, maxSizeRem)}rem`,
+    ].join(', ')})`;
+  };
+}
+
+/**
+ * PostCSS設定
+ * @type {Config}
+ */
+const config: Config = {
+  plugins: {
+    '@yuheiy/postcss-custom-functions': {
+      functions: {
+        '--fluid': fluid(),
+      },
+    },
+  },
+};
+
+export default config;
---
+++ vitest.config.ts
@@ -0,0 +1,8 @@
+import { getViteConfig } from 'astro/config';
+
+export default getViteConfig({
+  // @ts-expect-error Type error caused by different Vite versions used by Vitest and Astro dependencies
+  test: {
+    includeSource: ['src/**/*.{js,ts,jsx,tsx}'],
+  },
+});
